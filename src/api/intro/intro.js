e571013567edb0336f2418c682874b637a6c3a0f
Tom Bouillut <tom@croll.fr>
2020-01-31 14:53:51 -0300

    Intro SlideShow changing image uri and renderItem construction

--- src/api/intro/intro.js
+++ src/api/intro/intro.js
@@ -1,6 +1,6 @@
 import React , { Component } from 'react';
 import SafeAreaView from 'react-native-safe-area-view';
-import { TouchableOpacity,Image, StyleSheet, View, Text, ActivityIndicator, Platform } from 'react-native';
+import { ImageBackground, TouchableOpacity,Image, StyleSheet, View, Text, ActivityIndicator, Platform } from 'react-native';
 import I18n from "../../utils/i18n";
 import AppIntroSlider from 'react-native-app-intro-slider';
 const slides = [
@@ -8,28 +8,28 @@ const slides = [
     key: 'p1',
     title: I18n.t("Welcome","Welcome"),
     text: I18n.t("Invite","We invite you to explore a story."),
-    image: require('../../../assets/intro/bow-slider-bg_c01.jpg'),
+    image: '../../../assets/intro/bow-slider-bg_c01.jpg',
     backgroundColor: '#59b2ab',
   },
   {
     key: 'p2',
     title:  I18n.t("Each_story","Each story"),
     text: I18n.t("discover","has many walls to discover"),
-    image: require('../../../assets/intro/bow-slider-bg_c02.jpg'),
+    image: '../../../assets/intro/bow-slider-bg_c02.jpg',
     backgroundColor: '#febe29',
   },
   {
     key: 'p3',
     title: I18n.t("Each_wall","Each wall"),
     text: I18n.t("reveal","has many secrets to reveal"),
-    image: require('../../../assets/intro/bow-slider-bg_c03.jpg'),
+    image: '../../../assets/intro/bow-slider-bg_c03.jpg',
     backgroundColor: '#22bcb5',
   },
   {
     key: 'p4',
     title: I18n.t("Enjoy","Enjoy it"),
     text: I18n.t("Press","Press done to start"),
-    image: require('../../../assets/intro/bow-slider-bg_c04.jpg'),
+    image: '../../../assets/intro/bow-slider-bg_c04.jpg',
     backgroundColor: '#22bcb5',
   }
 ];
@@ -37,11 +37,8 @@ const styles = StyleSheet.create({
   mainContent: {
     flex: 1,
     alignItems: 'center',
+    backgroundColor: 'transparent',
     justifyContent: 'space-around',
-  },
-  image: {
-    width: 320,
-    height: 320,
   },
   text: {
     color: 'black',
@@ -66,15 +63,14 @@ export default class Intro extends Component {
   renderItem = ({ item }) => {
     return (
       <View style={styles.slide}>
-        <Text style={styles.title}>{item.title}</Text>
-        <Image source={item.image} />
-        <Text style={styles.text}>{item.text}</Text>
+        <ImageBackground source={{uri: item.image}} style={{width: '100%', height: '100%'}}>
+          <Text style={styles.title}>{item.title}</Text>
+          <Text style={styles.text}>{item.text}</Text>
+        </ImageBackground>
       </View>
     );
   }
-  onDone = () => {
-    this.props.navigation.navigate('Stories');
-  }
+  onDone = () => this.props.navigation.navigate('Stories');
   render() {
 
       return (
