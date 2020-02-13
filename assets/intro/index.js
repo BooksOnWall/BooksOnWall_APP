0b6a5402af966f538e5cbb85642b2b819ad873b5
Tom Bouillut <tom@croll.fr>
2020-02-13 14:31:05 -0300

    Intro slide Fixed background image index

---
+++ assets/intro/index.js
@@ -0,0 +1,6 @@
+export const Images = {
+   image1: require('./bow-slider-bg_c01.jpg'),
+   image2: require('./bow-slider-bg_c02.jpg'),
+   image3: require('./bow-slider-bg_c03.jpg'),
+   image4: require('./bow-slider-bg_c04.jpg'),
+ };
--- src/api/intro/intro.js
+++ src/api/intro/intro.js
@@ -3,33 +3,35 @@ import SafeAreaView from 'react-native-safe-area-view';
 import { ImageBackground, TouchableOpacity,Image, StyleSheet, View, Text, ActivityIndicator, Platform } from 'react-native';
 import I18n from "../../utils/i18n";
 import AppIntroSlider from 'react-native-app-intro-slider';
+import { Images } from '../../../assets/intro';
+
 const slides = [
   {
     key: 'p1',
     title: I18n.t("Welcome","Welcome"),
     text: I18n.t("Invite","We invite you to explore a story."),
-    image: '../../../assets/intro/bow-slider-bg_c01.jpg',
+    image: Images['image1'],
     backgroundColor: '#59b2ab',
   },
   {
     key: 'p2',
     title:  I18n.t("Each_story","Each story"),
     text: I18n.t("discover","has many walls to discover"),
-    image: '../../../assets/intro/bow-slider-bg_c02.jpg',
+    image: Images['image2'],
     backgroundColor: '#febe29',
   },
   {
     key: 'p3',
     title: I18n.t("Each_wall","Each wall"),
     text: I18n.t("reveal","has many secrets to reveal"),
-    image: '../../../assets/intro/bow-slider-bg_c03.jpg',
+    image: Images['image3'],
     backgroundColor: '#22bcb5',
   },
   {
     key: 'p4',
     title: I18n.t("Enjoy","Enjoy it"),
     text: I18n.t("Press","Press done to start"),
-    image: '../../../assets/intro/bow-slider-bg_c04.jpg',
+    image: Images['image4'],
     backgroundColor: '#22bcb5',
   }
 ];
@@ -63,9 +65,10 @@ export default class Intro extends Component {
   renderItem = ({ item }) => {
     return (
       <View style={styles.slide}>
-        <ImageBackground source={{uri: item.image}} style={{width: '100%', height: '100%'}}>
+        <ImageBackground source={item.image} style={{width: '100%', height: '100%'}}>
           <Text style={styles.title}>{item.title}</Text>
           <Text style={styles.text}>{item.text}</Text>
+          <Image source={item.image} />
         </ImageBackground>
       </View>
     );
