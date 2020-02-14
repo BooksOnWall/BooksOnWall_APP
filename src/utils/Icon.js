22ed3fec697cbe07804589d58de0aa81e5544b3c
Tom Bouillut <tom@croll.fr>
2020-02-14 18:02:41 -0300

    Added Custom Icon lib

---
+++ src/utils/Icon.js
@@ -0,0 +1,3 @@
+import {createIconSetFromIcoMoon} from 'react-native-vector-icons';
+import icoMoonConfig from '../../assets/nav/selection.json';
+export default createIconSetFromIcoMoon(icoMoonConfig);
