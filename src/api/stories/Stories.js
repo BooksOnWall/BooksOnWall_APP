b8edbded8cd02455be13eeb04b2827aab99d1731
cs <cristobalseverin@gmail.com>
2020-02-25 19:30:12 -0300

    merge fix

--- src/api/stories/Stories.js
+++ src/api/stories/Stories.js
@@ -183,11 +183,8 @@ export default class Stories extends Component {
             containerStyle={{ backgroundColor: '#D8D8D8', justifyContent: 'space-around', borderWidth: 0, paddingTop: 25, paddingBottom: 25}}
             centerComponent={<Icon name='bow-logo' style={styles.logo}/>}
             />
-<<<<<<< HEAD
-          <Card style={styles.card} containerStyle={{padding: 0, margin: 0, borderWidth: 0}}>
-=======
+
           <Card style={styles.card} containerStyle={{padding: 0, margin: 0, borderWidth: 0, backgroundColor: '#8c8c8c'}}>
->>>>>>> 7dd219f78322402c86d2f5901c301d0e352762ab
               <ListStories stories={stories} navigate={navigate} />
           </Card>
         </SafeAreaView>
